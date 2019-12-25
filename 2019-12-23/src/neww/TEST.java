0baead4914697ec4eb1643c80e4c655f068a0efb
EnoshTs <enosh.tsur@haaretz.co.il>
2019-12-25 13:47:58 +0200

    added

---
+++ 2019-12-23/src/neww/TEST.java
@@ -0,0 +1,11 @@
+package neww;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1> Add new text\n2> Delete last\n3> Delete first\n4> Print\n5> Exit");
+
+	}
+
+}
---
+++ 2019-12-25/src/collections/Program2.java
@@ -0,0 +1,11 @@
+package collections;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1> Add new text\n2> Delete last\n3> Delete first\n4> Print\n5> Exit");
+
+	}
+
+}
